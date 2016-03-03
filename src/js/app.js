4651b5d3d6c8ebb4446ee414dd49d36cc7d22113
Elliot Bonneville <elliot.bonneville@gmail.com>
2016-03-02 23:58:10 -0500

    Make render calls occur a max of once every 14ms

--- src/js/app.js
+++ src/js/app.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 
 import Display from "~/renderer/display";
 import Map from "~/core/map";
+import Rect from "~/utils/rect";
 
 function create(container) {
 	window.map = new Map();
 	window.display = new Display({map});
+	window.Rect = Rect;
 };
 
 module.exports = {
--- src/js/renderer/buffer.js
+++ src/js/renderer/buffer.js
@@ -1,5 +1,5 @@
 import EventEmitter from "events";
-import {throttle} from "lodash";
+import {debounce} from "lodash";
 
 import Cell from "./cell";
 import Point from "~/utils/point";
@@ -15,9 +15,15 @@ let buffer = Object.assign({}, EventEmitter.prototype, {
 	registerView: () => {
 		return {
 			cells: _cells, 
-			render: throttle(() => buffer.emit("render"), 1000 / 60)
+			render: buffer.render
 		};
 	},
+});
+
+buffer.render = debounce(() => {
+	buffer.emit("render");
+}, 14, {
+	trailing: true
 });
 
 for (let x = 0; x < buffer.settings.width; x++) {
